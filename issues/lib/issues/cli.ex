cac1bcbf92a2291ef44bba6bbd1b7b8d4e82af41
Nikola Novakovic <nikola.novakovic9@gmail.com>
2016-04-11 10:56:40 +0200

    Start implementing tests

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -25,13 +25,13 @@ defmodule Issues.CLI do
     parse = OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
 
     case parse do
-      {[help: true], _, _} ->
+      {[help: true], _, _}
         -> :help
         
-      {_,[ user, project, count ], _} ->
-        -> { user, project, count }
+      {_,[ user, project, count ], _}
+        -> { user, project, String.to_integer(count) }
 
-      {_, [user, project], _} ->
+      {_, [user, project], _}
         -> {user, project, @default_count}
 
       _ -> :help
