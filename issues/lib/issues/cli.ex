53df41d5ecfd2b441c44c8786307058643726d9f
Nikola Novakovic <nikola.novakovic9@gmail.com>
2016-04-13 11:26:10 +0200

    Add a table formatter

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -7,11 +7,15 @@ defmodule Issues.CLI do
   to various functions that end up generating a table of 
   the last _n_ issues in a github project
   """
+  
+  import Issues.TableFormatter, only: [ print_table_for_columns: 2 ]
 
   def run(argv) do
     argv
     |> parse_args
     |> process
+    |> convert_to_list_of_hashdicts
+    |> sort_into_ascending_order
   end
 
   @doc """
@@ -48,9 +52,13 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response
+    |> convert_to_list_of_hashdicts
+    |> sort_into_ascending_order
+    |> Enum.take(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
   end
 
   def decode_response({:ok, body}), do: body
@@ -64,5 +72,9 @@ defmodule Issues.CLI do
   def convert_to_list_of_hashdicts(list) do
     list
     |> Enum.map(&Enum.into(&1, HashDict.new))
+  end
+
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues, fn i1, i2 -> i1["created_at"] <= i2["created_at"] end
   end
 end
