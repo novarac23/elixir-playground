9dca1d3a8c7f5c5c14f448182b0a0b26a01dea38
Nikola Novakovic <nikola.novakovic9@gmail.com>
2016-04-28 08:48:29 +0200

    Add format status for statistics purposes

--- sequence/lib/server.ex
+++ sequence/lib/server.ex
@@ -1,6 +1,10 @@
 defmodule Sequence.Server do
   use GenServer
 
+  def format_status(_reason, [_pdict, state]) do
+    [data: [{'State', "Myyyyy current state is '#{inspect state}', and I am happy with that!"}]]
+  end
+
   def handle_call(:next_number, _from, current_number) do
     {:reply, current_number, current_number + 1}
   end
@@ -16,4 +20,5 @@ defmodule Sequence.Server do
   def handle_cast({:increment_number, delta}, current_number) do
     {:noreply, current_number + delta}
   end
+
 end
