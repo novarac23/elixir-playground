bf29f631e0b68b0f8cc4ed7660276285d3e4e57d
Nikola Novakovic <nikola.novakovic9@gmail.com>
2016-05-09 21:43:16 +0200

    Add an initalizer for sequnce tests

--- sequence/test/sequence_test.exs
+++ sequence/test/sequence_test.exs
@@ -2,6 +2,11 @@ defmodule SequenceTest do
   use ExUnit.Case
   import Sequence.Server
 
+  setup do
+    :sys.replace_state(Sequence.Server, fn {_old_state, pid} -> {Application.get_env(:sequence, :initial_number), pid} end)
+    :ok
+  end 
+
   test "next_number" do
     init_number = Application.get_env(:sequence, :initial_number)
 
@@ -13,6 +18,6 @@ defmodule SequenceTest do
     init_number = Application.get_env(:sequence, :initial_number)
     increment_number 1
 
-    assert next_number == init_number + 3
+    assert next_number == init_number + 1
   end
 end
