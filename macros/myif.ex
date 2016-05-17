b5eb6f1bf620ed780fed3b724ee06c1c0cb43208
Nikola Novakovic <nikola.novakovic9@gmail.com>
2016-05-17 10:13:18 +0200

    Add a couple of macros

---
+++ macros/myif.ex
@@ -0,0 +1,52 @@
+defmodule My do
+  defmacro if(condition, clauses) do
+    do_clause = Keyword.get(clauses, :do, nil)
+    else_clause = Keyword.get(clauses, :else, nil)
+
+    quote do
+      case unquote(condition) do
+        val when val in [false, nil] -> unquote(else_clause)
+        _ -> unquote(do_clause)
+      end
+    end
+  end
+
+  defmacro unless(condition, clauses) do
+    do_clause = Keyword.get(clauses, :do, nil)
+    else_clause = Keyword.get(clauses, :else, nil)
+    
+    quote do
+      if unquote(condition) do
+        unquote(else_clause)
+      else
+        unquote(do_clause)
+      end
+    end
+  end
+
+  defmacro times_n(value) do
+    name = :"times_#{value}"
+    quote do
+      def unquote(name)(x), do: unquote(value) * x 
+    end
+  end
+end
+
+defmodule Test do
+  require My
+
+  My.if 1==2 do
+    IO.puts "Expression is truthy"
+  else
+    IO.puts "Expressions is falsy"
+  end
+
+  My.unless 1==1 do 
+    IO.puts "Inside FIRST branch"
+  else
+    IO.puts "Inside SECOND branch"
+  end
+
+  My.times_n(3)
+  My.times_n(4)
+end
