3ec22bf533958f979c5966a9e542f0306522fa05
Nikola Novakovic <nikola.novakovic9@gmail.com>
2016-05-23 10:28:20 +0200

    WIP

--- behavior_example.exs
+++ behavior_example.exs
@@ -1,12 +1,17 @@
 defmodule TestBehavior do
   use Behaviour
   
-  defcallback crazy() :: integer
+  defcallback say_hello(String.t) :: any
 end
 
 
 defmodule Test do
   @behaviour TestBehavior 
 
-  def crazy(), do: 40
+  def say_hello(param) do
+    IO.puts param
+  end
 end
+
+Test.say_hello "nik"
+Test.say_hello 2
