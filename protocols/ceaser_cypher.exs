a761239c22c41f5b10d955ada5416dff60eced28
Nikola Novakovic <nikola.novakovic9@gmail.com>
2016-05-23 10:05:51 +0200

    WIP

---
+++ protocols/ceaser_cypher.exs
@@ -0,0 +1,30 @@
+defprotocol Caesar do
+  def encrypt(string, shift)
+
+  def rot13(string)
+end
+
+defimpl Caesar, for: [BitString, List] do
+  @letter_length 26
+  @upper_case_start 65
+  @upper_case_end 90
+  @lower_case_start 97
+  @lower_case_end 122
+
+  defp is_upper_case?(i) do
+    i >= @upper_case_start && i <= @upper_case_end
+  end
+
+  defp is_lower_case?(i) do
+    i >= @lower_case_start && i <= @lower_case_end
+  end
+
+  def encrypt(string, shift) do
+    string
+    |> to_char_list
+    |> Enum.map(fn i -> shift_car(i, shift) end)
+    |> List.to_string
+  end
+
+  defp shift_car
+end
