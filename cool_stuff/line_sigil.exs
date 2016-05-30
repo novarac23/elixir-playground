98f7ff7bcf83ca03242689e7ba254cd2567c33b9
Nikola Novakovic <nikola.novakovic9@gmail.com>
2016-05-30 16:57:00 +0200

    Add more sigils

--- cool_stuff/line_sigil.exs
+++ cool_stuff/line_sigil.exs
@@ -13,9 +13,30 @@ defmodule CommaSigil do
   end
 end
 
+defmodule FloatSigil do
+  def sigil_m(lines, _opts) do
+    lines 
+    |> String.rstrip 
+    |> String.split("\n") 
+    |> Enum.map(&(&1 |> String.split(",")))
+    |> Enum.map(&(check_for_float(&1)))
+  end
+
+  defp check_for_float(lines) do
+    lines
+    |> Enum.map(fn el ->
+                  case Float.parse(el) do
+                    {float, _} -> float
+                    _ -> el
+                  end
+       end)  
+  end
+end
+
 defmodule Example do
   import LineSigil
   import CommaSigil
+  import FloatSigil
 
   def lines do
     ~l"""
@@ -28,6 +49,13 @@ defmodule Example do
     ~k"""
     a,b,c
     1,2,3
+    """
+  end
+
+  def floatz do
+    ~m"""
+    1,2.22,3
+    a,b,q
     """
   end
 end
